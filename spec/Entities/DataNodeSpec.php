ff8f9202f6293500f7e12b416f55b08438347c7b
Chris Levy <lev@levofski.co.uk>
2015-02-28 16:43:42 +0000

    Fleshing out even more spec for DataNode

--- spec/Entities/DataNodeSpec.php
+++ spec/Entities/DataNodeSpec.php
@@ -12,11 +12,61 @@ class DataNodeSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Web2CV\Entities\DataNode');
     }
-    
+
+    /** Primitive Children */
+    function it_can_contain_a_primitive()
+    {
+        $this->set("string");
+        $this->get()->shouldReturn(["string"]);
+        $this->get(1)->shouldReturn("string");
+    }
+
+    function it_can_contain_a_named_primitive()
+    {
+        $this->set("string", 'child');
+        $this->get()->shouldReturn(['child' => "string"]);
+        $this->get('child')->shouldReturn("string");
+    }
+
+    function it_can_contain_multiple_primitives()
+    {
+        $this->set("string");
+        $this->get()->shouldReturn(["string"]);
+        $this->set(99);
+        $this->get()->shouldReturn(["string", 99]);
+        $this->get(1)->shouldReturn("string");
+        $this->get(2)->shouldReturn(99);
+    }
+
+    function it_can_contain_multiple_named_primitives()
+    {
+        $this->set("string", 'child1');
+        $this->get()->shouldReturn(['child1' => "string"]);
+        $this->set(99, 'child2');
+        $this->get()->shouldReturn(['child1' => "string", 'child2' => 99]);
+        $this->get('child1')->shouldReturn("string");
+        $this->get('child2')->shouldReturn(99);
+    }
+
+    function it_can_contain_multiple_named_and_unnamed_primitives()
+    {
+        $this->set("string", 'child1');
+        $this->get()->shouldReturn(['child1' => "string"]);
+        $this->set(99);
+        $this->get()->shouldReturn(['child1' => "string", 99]);
+        $this->set(3.14, 'child2');
+        $this->get()->shouldReturn(['child1' => "string", 99, 'child2' => 3.14]);
+        $this->get('child1')->shouldReturn("string");
+        $this->get(1)->shouldReturn(99);
+        $this->get('child2')->shouldReturn(3.14);
+    }
+
+    /** DataNode children */
     function it_can_contain_a_data_node(DataNode $dataNode)
     {
 		$this->set($dataNode);
 		$this->get()->shouldReturn([$dataNode]);
+        $this->get(1)->shouldReturn($dataNode);
     }
     
     function it_can_contain_a_named_data_node(DataNode $dataNode)
@@ -58,4 +108,40 @@ class DataNodeSpec extends ObjectBehavior
 		$this->get(1)->shouldReturn($dataNode2);
 		$this->get('child2')->shouldReturn($dataNode3);
     }
+
+    /** Mixed children */
+    function it_can_contain_multiple_mixed_data(DataNode $dataNode)
+    {
+        $this->set($dataNode);
+        $this->get()->shouldReturn([$dataNode]);
+        $this->set("string");
+        $this->get()->shouldReturn([$dataNode, "string"]);
+        $this->get(1)->shouldReturn($dataNode);
+        $this->get(2)->shouldReturn("string");
+    }
+
+    function it_can_contain_multiple_named_mixed_data(DataNode $dataNode)
+    {
+        $this->set($dataNode, 'child1');
+        $this->get()->shouldReturn(['child1' => $dataNode]);
+        $this->set("string", 'child2');
+        $this->get()->shouldReturn(['child1' => $dataNode, 'child2' => "string"]);
+        $this->get('child1')->shouldReturn($dataNode);
+        $this->get('child2')->shouldReturn("string");
+    }
+
+    function it_can_contain_multiple_named_and_unnamed_mixed_data(DataNode $dataNode)
+    {
+        $this->set($dataNode, 'child1');
+        $this->get()->shouldReturn(['child1' => $dataNode]);
+        $this->set("string");
+        $this->get()->shouldReturn(['child1' => $dataNode, "string"]);
+        $this->set(99, 'child2');
+        $this->get()->shouldReturn(['child1' => $dataNode, "string", 'child2' => 99]);
+        $this->get('child1')->shouldReturn($dataNode);
+        $this->get(1)->shouldReturn("string");
+        $this->get('child2')->shouldReturn(99);
+    }
+
+
 }
